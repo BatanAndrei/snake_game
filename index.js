b47a94bc4c99856e89cbdc85934946a872499abe
Batan Andrei <yabatan777@gmail.com>
2023-01-24 20:46:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ function initsquad(){
     let squad = document.querySelector(".test");
 
     squad.addEventListener('click', function(){
-    alert("Check alfa!");
+    //alert("Check alfa!");
+    initNextSquad();
 });
+    
+   
+function initNextSquad(){
+    let nextSquad = `<div class='test_next'></div>`;
+
+    squad.innerHTML = nextSquad;
+    console.log(squad)
+}
 }
 
 
